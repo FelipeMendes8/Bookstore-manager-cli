23565871d24244d041042012cd73f9d4c6ad09ea
Felipe Mendes <felipemendesoliveira8@gmail.com>
2026-08-09 22:12:16 -0300

    Corrigindo a estrutura de sub-menus

--- src/menus/menu-autor.ts
+++
@@ -1,9 +0,0 @@
-export class MenuAutor {
-  public iniciar(): void {
-    this.exibirMenu();
-  }
-
-  private exibirMenu(): void {
-    console.log('Exibir menu autor');
-  }
-}
--- src/menus/menu-principal.ts
+++ src/menus/menu-principal.ts
@@ -1,11 +1,5 @@
 import type { Interface } from 'node:readline/promises';
 
-import { MenuAutor } from './menu-autor';
-import { MenuCliente } from './menu-cliente';
-import { MenuEmprestimo } from './menu-emprestimo';
-import { MenuLivro } from './menu-livro';
-import { MenuRelatorios } from './menu-relatorios';
-
 export async function exibirMenuPrincipal(terminal: Interface): Promise<void> {
   //Obs: Aqui seria bom validar o login do funcionário
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
@@ -16,40 +10,35 @@ export async function exibirMenuPrincipal(terminal: Interface): Promise<void> {
     console.log('');
     console.log('Gerencie a livraria: registre livros, autores e faça empréstimos de livros.\n');
 
-    console.log('1- Livros\n2- Autores\n3- Clientes\n4- Empréstimos\n5- Relatórios\n0- Sair');
+    console.log('1- Autores\n2- Livros\n3- Clientes\n4- Empréstimos\n5- Relatórios\n0- Sair');
 
     const opcao = await terminal.question('Digite uma opção: ');
 
     switch (opcao) {
       case '1': {
-        console.log('Acessando menu de livros...');
-        const livro = new MenuLivro();
-        livro.iniciar();
+        console.log('Acessando menu dos autores...');
+
         break;
       }
 
       case '2': {
-        console.log('Acessando menu dos autores...');
-        const autor = new MenuAutor();
-        autor.iniciar();
+        console.log('Acessando menu de livros...');
+
         break;
       }
       case '3': {
         console.log('Acessando menu de clientes...');
-        const cliente = new MenuCliente();
-        cliente.iniciar();
+
         break;
       }
       case '4': {
         console.log('Acessando menu de empréstimos...');
-        const emprestimo = new MenuEmprestimo();
-        emprestimo.iniciar();
+
         break;
       }
       case '5': {
         console.log('Acessando menu de relatórios...');
-        const relatorios = new MenuRelatorios();
-        relatorios.iniciar();
+
         break;
       }
       case '0': {
